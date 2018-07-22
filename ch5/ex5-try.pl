1dafb83c5d5b57d909c3e9a954cf8dd5800e08fe
ecs-e-1 <ben@debug010000002015.local.alipay.net>
2018-07-22 13:55:06 +0800

    vim's pathogen+vundel xtps+perlsupport under git clone

--- ch5/ex5-try.pl
+++ ch5/ex5-try.pl
@@ -4,15 +4,31 @@ use utf8;
 
 #my	@ARGV = qw# larry moe curly #;          # oh, my  
 @ARGV = qw#larry moe curly#;
-unshift @ARGV ,'ex5-1.pl';                      
+unshift @ARGV ,'aka';                      
 while ( <> ) {
 	chomp;
 	print "It was $_ that I saw!  \n";
 }
 
+
+
 =begin  BlockComment  # BlockCommentNo_1
 
+my	@array = <STDIN>;
+print "@array\n";
+print @array;
+
 =end    BlockComment  # BlockCommentNo_1
+
+=cut
+
+=begin  BlockComment  # BlockCommentNo_2
+
+
+print (2+3)*5; print "\n";
+print ((2+3)*5);
+
+=end    BlockComment  # BlockCommentNo_2
 
 =cut
 
---
+++ ch5/xtp.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  xtp.c
+ *
+ *    Description:  4 test vundle-xtps vs pathogen-perl_support
+ *
+ *        Version:  1.0
+ *        Created:  07/22/2018 01:46:33 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+for ( i = 0; i < len; i++ ) {
+	/* cursor */
+}
