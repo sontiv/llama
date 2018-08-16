ab2685d1fc5d71e29774eaefebd704d2374fde5b
flinter <flinteros@qq.com>
2018-08-17 00:15:46 +0800

    git ignore some

--- ch5/ex5-try_8.pl
+++ ch5/ex5-try_8.pl
@@ -2,7 +2,15 @@
 use 5.014;
 use utf8;
 
-if (! open LOG, ">>", "logfile") {
-	
-
+if (! open LOG, ">", "log") {               #chown root:root logfile 
+	die "Cannot create logfile: $!";
 }
+close LOG;
+
+if ( @ARGV < 2 ) {
+	warn "Not enough arguments\n";
+	die "$0: Not enough arguments\n";
+}
+
+9/<STDIN> or die "anything wrong? $!";
+
